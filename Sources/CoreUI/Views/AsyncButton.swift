dbfbcc713b07f1f83447e8926d657264a9ec3f93
Pavel Vaitsikhouski <pavel.vaitsikhouski@gmail.com>
2024-09-14 13:23:32 +0200

    Update AsyncButton

--- Sources/CoreUI/Views/AsyncButton.swift
+++ Sources/CoreUI/Views/AsyncButton.swift
@@ -8,6 +8,10 @@
 import SwiftUI
 
 public struct AsyncButton<Label>: View where Label: View {
+    @Environment(\.asyncButtonColorPalette) private var colorPalette
+
+    @State private var isLoading: Bool = false
+
     var perform: () async -> Void
     var label: () -> Label
 
@@ -19,10 +23,39 @@ public struct AsyncButton<Label>: View where Label: View {
     public var body: some View {
         Button {
             Task { @MainActor in
+                isLoading = true
                 await perform()
+                isLoading = false
             }
         } label: {
-            label()
+            ZStack {
+                label().opacity(isLoading ? 0 : 1)
+                ProgressView()
+                    .tint(colorPalette.progressView)
+                    .opacity(isLoading ? 1 : 0)
+            }
         }
+    }
+}
+
+// MARK: - EnvironmentKey
+
+private struct AsyncButtonColorPaletteKey: EnvironmentKey {
+    static let defaultValue: AsyncButtonColorPalette = AsyncButtonColorPalette()
+}
+
+public struct AsyncButtonColorPalette: Sendable, Equatable, Hashable {
+    var progressView: Color
+
+    public init(progressView: Color = .black) {
+        self.progressView = progressView
+    }
+}
+
+public extension EnvironmentValues {
+
+    var asyncButtonColorPalette: AsyncButtonColorPalette {
+        get { self[AsyncButtonColorPaletteKey.self] }
+        set { self[AsyncButtonColorPaletteKey.self] = newValue }
     }
 }
